bc6a5a6c53dc65fa5778e301145993c6f8645993
viggodb <vdebreij@gmail.com>
2023-11-10 15:24:00 +0100

    view bitmap map k v

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
 module Main where
-
+import Data.Map
 import Controller
 import Model
 import View
@@ -9,14 +9,15 @@ import Graphics.Gloss.Interface.IO.Game
 main :: IO ()
 -- main = Graphics.Gloss.Interface.IO.Game.playIO (Graphics.Gloss.Interface.IO.Game.InWindow "Counter" (400, 400) (0, 0)) -- Or FullScreen
 main = do  
-    s <- loadBMP "tiny_ship.bmp"
-    s2 <- loadBMP "tiny_ship_bw.bmp"
+    s <- loadBMP "tiny_ship.bmp" 
     pea <- loadBMP "pea.bmp"
+    
+    --splosieframes <- mapM loadBMP ["splosieframes/splosieframe1.bmp","splosieframes/splosieframe2.bmp"]
     Graphics.Gloss.Interface.IO.Game.playIO Graphics.Gloss.Interface.IO.Game.FullScreen
               Graphics.Gloss.Interface.IO.Game.black            -- Background color
               60               -- Frames per second
               initialState     -- Initial state
-              (view [s,s2,pea])          -- View function
+              (view (Data.Map.fromList [("ship",s),("pea",pea)]))          -- View function
               input            -- Event function
               step             -- Step function
               
--- src/View.hs
+++ src/View.hs
@@ -1,28 +1,28 @@
 -- | This module defines how to turn
 --   the game state into a picture
 module View where
-
+import Data.Map 
 import Graphics.Gloss
 import Model
 import Controller
 
-view :: [Picture] -> GameState -> IO Picture
+view :: Map String Picture -> GameState -> IO Picture
 view p gstate= return (pics p gstate)
 
 
-pics :: [Picture] -> GameState -> Picture
-pics (x1:_:peabullet:xs1) gstate
+pics :: Map String Picture -> GameState -> Picture
+pics picturemap gstate
   | status gstate == StartScreen = Translate (-200) (-50) (color green (text "Start!"))
   -- | status gstate == GameOver = Translate (-200) (-50) (color green (text "Start!"))
   | otherwise =
   Pictures ([
-    Translate x y (x1) -- Player
+    Translate x y (picturemap ! "ship") -- Player
       --Translate x y (color green (Circle s)) -- Player
     , Translate 30 30 (viewPure gstate) -- info to show
     , Translate 0 400 (scale 0.5 0.5 (viewScore gstate))] -- score
-    ++ entityPics bts [peabullet] -- player bullets
-    ++ entityPics (enemies gstate)[peabullet] -- render enemies
-    ++ entityPics (flatten (map bullets (enemies gstate))) [peabullet] -- enemy bullets
+    ++ entityPics bts picturemap -- player bullets
+    ++ entityPics (enemies gstate) picturemap -- render enemies
+    ++ entityPics (flatten (Prelude.map bullets (enemies gstate))) picturemap -- enemy bullets
     )
   where
     (Pt x y, s) = hitbox (player gstate)
@@ -31,22 +31,22 @@ pics (x1:_:peabullet:xs1) gstate
 
 
 
-entityPics :: [Entity]-> [Picture]-> [Picture]
+entityPics :: [Entity]-> Map String Picture-> [Picture]
 entityPics [] _= []
-entityPics (entity:es) [ppp]= Translate x y pic : (entityPics es [ppp])
+entityPics (entity:es) picturemap= Translate x y pic : entityPics es picturemap
     where 
       s = snd (hitbox entity)
       (Pt x y) = fst (hitbox entity)
       pic = case entityType entity of
             -- enemies
-            Swarm -> (color red (Polygon [(0, 0), (s, 0), (s, s), (0, s)]))
-            Turret -> (color blue (Polygon [(0, 0), (s, 0), (s, s), (0, s)]))
-            Worm -> (color yellow (Polygon [(0, 0), (s, 0), (s, s), (0, s)]))
-            Boss -> (color red (Polygon [(0, 0), (s, 0), (s, s), (0, s)]))
+            Swarm -> color red (Polygon [(0, 0), (s, 0), (s, s), (0, s)])
+            Turret -> color blue (Polygon [(0, 0), (s, 0), (s, s), (0, s)])
+            Worm -> color yellow (Polygon [(0, 0), (s, 0), (s, s), (0, s)])
+            Boss -> color red (Polygon [(0, 0), (s, 0), (s, s), (0, s)])
             -- bullets
-            Pea -> (scale 0.1 0.1 ppp)
-            Rocket -> (color yellow (Circle s))
-            Laserbeam -> (color cyan (Line [(0, 0), (s, 0)]))
+            Pea -> scale 0.1 0.1 (picturemap ! "pea")
+            Rocket -> color yellow (Circle s)
+            Laserbeam -> color cyan (Line [(0, 0), (s, 0)])
             -- _ -> Blank -- is this necessary?
             Explosion -> color azure (Circle s)
 
