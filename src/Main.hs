823552f37adfd32911abb6281cc9d6b48cd2c23e
viggodb <vdebreij@gmail.com>
2023-11-12 19:06:35 +0100

    turret

--- src/Main.hs
+++ src/Main.hs
@@ -29,6 +29,7 @@ bitmaploadin = do
     failed <- loadBMP "bitmaps/Mission failed.bmp"
     pea <- loadBMP "bitmaps/pea.bmp"
     rocket <- loadBMP "bitmaps/Rocket.bmp"
+    turret <- loadBMP "bitmaps/turret.bmp"
     swarm <- loadBMP "bitmaps/swarm.bmp"
     worm <- loadBMP "bitmaps/worm.bmp"
     cloud <- loadBMP "bitmaps/Cloud.bmp"
@@ -42,5 +43,5 @@ bitmaploadin = do
                                "bitmaps/splosieframes/splosieframe6.bmp",
                                "bitmaps/splosieframes/splosieframe7.bmp"]
     let splosions2 = zip ["frame1","frame2","frame3","frame4","frame5","frame6","frame7"] splosions 
-    return (Data.Map.fromList ([("ship",s),("pea",pea),("swarm", swarm),("worm", worm),("failed",failed),("cloud",cloud),("moon",moon),("mountain",mountain),("rocket",rocket)]++splosions2))
+    return (Data.Map.fromList ([("ship",s),("pea",pea),("swarm", swarm),("worm", worm),("failed",failed),("cloud",cloud),("moon",moon),("mountain",mountain),("rocket",rocket),("turret",turret)]++splosions2))
               
